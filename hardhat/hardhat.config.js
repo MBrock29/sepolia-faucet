065be1d8365c62a5da8ece5a9c78ed263a7c703b
MBrock29 <mbrock19@gmail.com>
2023-11-05 01:29:50 -0500

    Hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,14 +3,14 @@ require("dotenv").config({ path: ".env" });
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
-const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
+    sepolia: {
       url: ALCHEMY_API_KEY_URL,
-      accounts: [ROPSTEN_PRIVATE_KEY],
+      accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
 };
